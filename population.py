c49aeb2bc0ea7e6514e5ebb8586e5d3a992971f9
Grant <grant_d_lohman@aol.com>
2023-10-05 20:26:03 -0500

    extracted population data using api

--- population.py
+++ population.py
@@ -1,5 +1,15 @@
+import requests
 
+url = 'https://api.census.gov/data/2021/pep/population?get=POP_2021,NAME&for=STATE'
+response = requests.get(url)
+data = response.json()
 def get_population(city, state_code):
     pass
-
-
+state = 'Nebraska'
+for inner_list in data:
+    # print(inner_list[0], inner_list[1])
+    pop = inner_list[0]
+    # print(pop)
+    if inner_list[1] == state:
+        print(state)
+        print(inner_list[0])
