285a6edd33d7460bf38a2f7cd0972a646105b89f
KRShee <KRShee@gmail.com>
2023-10-05 21:07:11 -0500

    LATEST Rep to work on our issues

--- climate.py
+++ climate.py
@@ -1,4 +1,4 @@
-
+#from geopy.geocoders import Nominatim
 # climate data
 
 def get_climate(city, state_code):
@@ -6,4 +6,21 @@ def get_climate(city, state_code):
     #request.get(.JSON)
     #response
     pass
+
+def get_longitude_latitude():
+        #get Longitute and latitude for the city and state entered
+        '''
+        geolocator = Nominatim(user_agent="specify_your_app_name_here")
+    location = geolocator.geocode("175 5th Avenue NYC")
+
+    print(location.address)
+    # Output: Flatiron Building, 175, 5th Avenue, Flatiron, New York, NYC, New York, ...
+
+    print((location.latitude, location.longitude))
+    # Output: (40.7410861, -73.9896297241625)
+
+    print(location.raw)
+    # Output: {'place_id': '9167009604', 'type': 'attraction', ...}
+        '''
+pass
 
--- location_info.py
+++ location_info.py
@@ -1,5 +1,5 @@
 import climate
-from geopy.geocoders import Nominatim
+
 
 
 '''Write action code as per main.py'''
@@ -13,7 +13,7 @@ def main():
     def get_location_info(location):
     
     # Climate
-        longitude_latitude = get_longitude_latitude(location)
+        longitude_latitude = climate.get_longitude_latitude(location)
         temp_mean_quarterly = climate.get_climate(longitude_latitude)
         
     # Population
@@ -27,22 +27,7 @@ def main():
     # Cost of living
     pass
 
-    def get_longitude_latitude():
-        #get Longitute and latitude for the city and state entered
-        '''
-        geolocator = Nominatim(user_agent="specify_your_app_name_here")
-    location = geolocator.geocode("175 5th Avenue NYC")
 
-    print(location.address)
-    # Output: Flatiron Building, 175, 5th Avenue, Flatiron, New York, NYC, New York, ...
-
-    print((location.latitude, location.longitude))
-    # Output: (40.7410861, -73.9896297241625)
-
-    print(location.raw)
-    # Output: {'place_id': '9167009604', 'type': 'attraction', ...}
-        '''
-    pass
 
     
 
