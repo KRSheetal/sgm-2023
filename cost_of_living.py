6825d46a6be5752986436b70d7f73073685dedc8
Grant <grant_d_lohman@aol.com>
2023-10-11 23:34:55 -0500

    function for gathering cost of living from api

--- cost_of_living.py
+++ cost_of_living.py
@@ -1,15 +1,19 @@
 import os
 import requests
-from geopy.geocoders import Nominatim
 
-city = 'Minneapolis'
-state_code = 'MN'
+import state_name_state_code_dictionary
+
 key = os.environ.get('COST_OF_LIVING_KEY')
-url = f'https://www.numbeo.com//api/city_cost_estimator?api_key={key}&query={city},%20{state_code},%20United%20States&members=1&children=0&include_rent=true&currency=USD'
-data = requests.get(url).json()
-def get_cost_of_living(city, state_code):
-    pass
 
-cost_of_living = data.get('overall_estimate')
+def get_cost_of_living(city, state):
+    city_name = city
+    state_name = state
+    for state in state_name_state_code_dictionary.states_and_state_codes:
+        state_code = state_name_state_code_dictionary.states_and_state_codes[state]
+        if state == state_name:
+            url = f'https://www.numbeo.com//api/city_cost_estimator?api_key={key}&query={city_name},%20{state_code},%20United%20States&members=1&children=0&include_rent=true&currency=USD'
+            data = requests.get(url).json()
+            cost_of_living = data.get('overall_estimate')
+            return cost_of_living
 
-print('{:.2f}'.format(cost_of_living))
+# print(get_cost_of_living('Minneapolis', 'Minnesota'))
