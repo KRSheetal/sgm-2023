52102cc49ff5b80d558500614e6593bb75885357
KRShee <KRShee@gmail.com>
2023-10-13 08:56:17 -0500

    Extracted Average temp by season-need to improvise

--- climate.py
+++ climate.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 import requests
+from statistics import mean
 
 # climate data
 climate_url_temp = 'https://climate-api.open-meteo.com/v1/climate?&start_date=2022-01-01&end_date=2022-12-31&models=FGOALS_f3_H&temperature_unit=fahrenheit&daily=temperature_2m_mean'
@@ -17,26 +18,46 @@ def get_climate(location):
     result_time = data['daily']['time']
     result_temp = data['daily']['temperature_2m_mean']
 
-    dict = {}
-        
-    for time_data, temp_data in zip(result_time, result_temp):
-        dict[time_data] = temp_data
-    
-    for time in dict.keys():
-        if time == '2022-03-30':
-            Q1 = dict[time]
-    
-        if time == '2022-06-30':
-            Q2 = dict[time]
-    
-        if time == '2022-09-30':
-            Q3 = dict[time]
-  
-        if time == '2022-12-30':
-            Q4 = dict[time]
+    #print(result_time.index('2022-03-31'))
+    # Q1_dates = result_time[0:90]
+    Q1_temp = result_temp[0:90]
+    Q1_mean = round(mean(Q1_temp))
+ 
+    Q1_time_location = result_time.index
+    # Q2_dates = result_time[91:180]
+    Q2_temp = result_temp[91:180]
+    Q2_mean = round(mean(Q2_temp))
+    # print(result_time.index('2022-09-30'))
+    # Q3_dates = result_time[180:272]
+    Q3_temp = result_temp[181:272]
+    Q3_mean = round(mean(Q3_temp))
+    # print(result_time.index('2022-12-31'))
+    # Q4_dates = result_time[272:364]
+    Q4_temp = result_temp[273:364]
+    Q4_mean = round(mean(Q4_temp))
 
-            
-    quarterly_temp = {'Spring':Q1, 'Summer': Q2, 'Fall': Q3, 'Winter': Q4}
+    
+    # dict = {}
+        
+    # for time_data, temp_data in zip(result_time, result_temp):
+    #     dict[time_data] = temp_data
+    
+    # for time in dict.keys():
+    #     if time == '2022-03-30':
+    #         Q1 = dict[time]
+    
+    #     if time == '2022-06-30':
+    #         Q2 = dict[time]
+    
+    #     if time == '2022-09-30':
+    #         Q3 = dict[time]
+  
+    #     if time == '2022-12-30':
+    #         Q4 = dict[time]
+
+     
+    quarterly_temp = {'Spring':Q1_mean, 'Summer': Q2_mean, 'Fall': Q3_mean, 'Winter': Q4_mean}
+    print(quarterly_temp)
 
     #TODO write code to extract temperature_2m_mean quarterly and return to location_info.py
 
@@ -54,11 +75,11 @@ def get_coordinates(location):
     return latitude, longitude
 
 
-# # added this for debugging purpose
-# location = ('minneapolis', 'minnesota')
+# added this for debugging purpose
+location = ('minneapolis', 'minnesota')
 
-# climate_extracted_data = get_climate(location)
-# #print (f'Data from climate.py {climate_extracted_data}') 
+climate_extracted_data = get_climate(location)
+#print (f'Data from climate.py {climate_extracted_data}') 
 
 
 
