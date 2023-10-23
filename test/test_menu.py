b6e982e38c4dafb0a8f7dad3131c5e5c8ab2526e
Grant <grant_d_lohman@aol.com>
2023-10-23 14:24:12 -0500

    reformatted file

--- test/test_menu.py
+++ test/test_menu.py
@@ -20,31 +20,25 @@ class TestMenu(TestCase):
         self.menu.add_option('a', 'aaaaa', a)
         self.menu.add_option('b', 'bbbbb', b)
 
-
     def test_add_option(self):
         self.assertDictEqual({'a': 'aaaaa', 'b': 'bbbbb'}, self.menu.text_descriptions)
         self.assertDictEqual({'a': self.a, 'b': self.b}, self.menu.functions)
-
 
     def test_is_valid(self):
         self.assertTrue(self.menu.is_valid('a'))
         self.assertTrue(self.menu.is_valid('b'))
 
-
     def test_is_not_valid(self):
         self.assertFalse(self.menu.is_valid('aa'))
         self.assertFalse(self.menu.is_valid('c'))
-
 
     def test_get_action(self):
         self.assertEqual(self.a, self.menu.get_action('a'))
         self.assertEqual(self.b, self.menu.get_action('b'))
 
-
     def test_get_action_returns_none_if_not_in_menu(self):
         self.assertIsNone(self.menu.get_action('aa'))
         self.assertIsNone(self.menu.get_action('c'))
-
 
     def test_str(self):
         menu_string = 'a: aaaaa\nb: bbbbb'
